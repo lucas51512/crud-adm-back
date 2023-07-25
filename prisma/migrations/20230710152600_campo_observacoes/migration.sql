337de10a1e16e3609db64bd9b4f2eb4ed55d3fa8
lucas51512 <lucasbrancalhao454@gmail.com>
2023-07-25 10:33:00 -0300

    .

--- prisma/migrations/20230710152600_campo_observacoes/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `observacoes` to the `Reuniao` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `reuniao` ADD COLUMN `observacoes` VARCHAR(191) NOT NULL;
