a4997e7eff7babfdb27ce56f55cedd96320bb82d
lucas51512 <lucasbrancalhao454@gmail.com>
2023-07-26 16:00:53 -0300

    Validações com Class Validator

--- src/reuniao/dto/create-reuniao.dto.ts
+++ src/reuniao/dto/create-reuniao.dto.ts
@@ -1,12 +1,22 @@
 import { Participante } from '@prisma/client';
+import { IsString, IsBoolean } from 'class-validator';
 
 export class CreateReuniaoDto {
+
+  @IsString()
   assuntoReuniao: string;
+
+  @IsString()
   descricaoReuniao: string;
+
+  @IsString()
   observacoes: string;
+
+  @IsBoolean()
   reuniaoDesmarcada: boolean;
   inicioReuniao?: Date | string;
   fimReuniao?: Date | string;
+  
   localIdFk?: number;
   listaParticipantes?: Participante[];
 }
