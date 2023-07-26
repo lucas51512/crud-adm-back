cf76a893f3456a1604e2022208c71d1b8ebe1b73
lucas51512 <lucasbrancalhao454@gmail.com>
2023-07-26 16:01:06 -0300

    Validações com Class Validator

--- src/participante/dto/create-participante.dto.ts
+++ src/participante/dto/create-participante.dto.ts
@@ -1,8 +1,20 @@
+import { IsInt, IsString, IsEmail, IsPhoneNumber, IsNumber, ValidateIf } from "class-validator";
 
 export class CreateParticipanteDto {
-  idParticipante?: number;
+
+  @IsInt()
+  idParticipante: number;
+
+  @IsString()
   nomeParticipante: string;
+
+  @IsEmail()
   emailParticipante: string;
+
+  @IsPhoneNumber()
   telefoneParticipante: string;
+
+  @IsNumber()
+  @ValidateIf((object, value) => value !== null)
   reuniaoIdFk?: number;
 }
--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, IsEmail } from "class-validator";
+
 export class CreateUsuarioDto {
+
+  @IsString()
   nomeUsuario: string;
+
+  @IsEmail()
   emailUsuario: string;
+
+  @IsString()
   senhaUsuario: string;
 }
